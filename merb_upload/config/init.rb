2c54412d36a8ec61fc36b2619c4e3850d01eb004
Max Aller <nanodeath@gmail.com>
2008-11-24 09:43:21 -0800

    Fresh installs...

---
+++ merb_upload/config/init.rb
@@ -0,0 +1,24 @@
+# Go to http://wiki.merbivore.com/pages/init-rb
+ 
+require 'config/dependencies.rb'
+ 
+use_orm :datamapper
+use_test :rspec
+use_template_engine :haml
+ 
+Merb::Config.use do |c|
+  c[:use_mutex] = false
+  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  
+  # cookie session store configuration
+  c[:session_secret_key]  = 'c5aad916e1e92a0d839fbb586c345cb36e06654a'  # required for cookie session store
+  c[:session_id_key] = '_merb_upload_session_id' # cookie session id key, defaults to "_session_id"
+end
+ 
+Merb::BootLoader.before_app_loads do
+  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+end
+ 
+Merb::BootLoader.after_app_loads do
+  # This will get executed after your app's classes have been loaded.
+end
