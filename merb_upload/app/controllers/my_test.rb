94aa862f6995fec31e913177545454dbd9a36016
Max Aller <nanodeath@gmail.com>
2008-11-24 20:41:17 -0800

    Little changes

--- merb_upload/app/controllers/my_test.rb
+++ merb_upload/app/controllers/my_test.rb
@@ -6,4 +6,11 @@ class MyTest < Application
     render
   end
   
+  def upload
+    puts params.inspect
+    uploads = Merb.root / 'uploads'
+    FileUtils.mkdir_p(uploads)
+    FileUtils.mv(params[:file][:tempfile].path, uploads / (params[:file][:filename] + '.' + rand(1000).to_s))
+    render :index
+  end  
 end
